8d7793147cd01275491788df9520ff3ba6e1b31a
Prasad Salunkhe <prasadsalunkhe1409@gmail.com>
2026-02-22 14:45:48 +0530

    Updated API URL for production

--- frontend_new/src/utils/axios.js
+++ frontend_new/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://digital-ration-backend.onrender.com/api",
 });
 
 // 🔐 Automatically attach token to every request
